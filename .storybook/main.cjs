6e231c9d93f13e362d253be5b7bacca59546a947
MarinaPereiraCardozo <cardozo.marina.p@gmail.com>
2022-10-15 21:02:55 -0300

    Add deploy-docs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, {configType}) => {
+    if(configType === "PRODUCTION") {
+      config.base = '/IgniteLabDesignSystem/'
+    }
+    return config
   }
 }
